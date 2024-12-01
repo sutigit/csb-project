da228fcf6a20ad8670b0d30ad2e5146bbfbc4887
sutigit <panyanilben@gmail.com>
2024-12-01 18:34:14 +0200

    add account creating functionality

--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -1,12 +1,53 @@
+import { useState } from 'react'
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+
+const backend_url = import.meta.env.VITE_BACKEND_URL
 
 // My components
 import Nav from '../components/Nav'
 
 export default function CreateAccount() {
+  const [isError, setIsError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSignup = (e: any) => {
+    e.preventDefault();
+
+    if (e.target[1].value !== e.target[2].value) {
+      setIsPasswordError(true)
+      return
+    }
+
+    fetch(`${backend_url}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: e.target[0].value,
+        password: e.target[1].value
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        Cookies.set('jwt', data.token)
+        Cookies.set('username', data.username)
+        navigate('/')
+      })
+      .catch(err => {
+        console.error(err)
+        setIsError(true)
+      })
+  }
+
   return (
     <>
       <Nav />
-      <form action="">
+      <form onSubmit={handleSignup}>
+        {isError && <span className='error-message'>Error creating account</span>}
+        {isPasswordError && <span className='error-message'>Passwords mismatch</span>}
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <input type="password" placeholder="Confirm Password" />
