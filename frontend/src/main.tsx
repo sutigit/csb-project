276617f055ee40ef115a1251bbb1fe9dacb2760d
sutigit <panyanilben@gmail.com>
2024-12-01 17:13:18 +0200

    code cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <StrictMode>
