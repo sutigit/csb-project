8978bf1526f3bba0f394163b0a6532ee57bf81a2
sutigit <panyanilben@gmail.com>
2024-12-01 13:15:19 +0200

    update frontend packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
-import './App.css'
+import './styles.css'
+
+import { useState } from 'react'
+
+// My components
+import BlogCard from './components/BlogCard'
+import { useEffect } from 'react'
+
+// Environment variables
+const backend_url = import.meta.env.VITE_BACKEND_URL
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    // fetch data from the server
+    fetch(`${backend_url}/blogs`)
+      .then(res => res.json())
+      .then(data => setBlogs(data))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <main>
-      <h1>Hello</h1>
+      <h1>Blogs</h1>
+      <section>
+        {blogs.map((blog: any) => (
+          <BlogCard key={blog.id} title={blog.title} content={blog.content} />
+        ))}
+      </section>
     </main>
   )
 }
