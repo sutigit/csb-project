20e23c484738c7ff29eecb2849cb2b499703e853
sutigit <panyanilben@gmail.com>
2024-12-01 16:32:33 +0200

    update to populate Blog components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,48 @@
 import './styles.css'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // My components
+import Nav from './components/Nav'
 import BlogCard from './components/BlogCard'
-import { useEffect } from 'react'
+
+interface Blog {
+  user_id: number
+  title: string
+  content: string
+}
 
 // Environment variables
 const backend_url = import.meta.env.VITE_BACKEND_URL
 
 function App() {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState<Blog[]>([])
 
   useEffect(() => {
-    // fetch data from the server
+
+    // fetch blogs from the server
     fetch(`${backend_url}/blogs`)
       .then(res => res.json())
-      .then(data => setBlogs(data))
+      .then(data => {
+        console.log(data)
+        setBlogs(data)
+      })
       .catch(err => console.error(err))
+      
   }, [])
 
   return (
-    <main>
-      <h1>Blogs</h1>
-      <section>
-        {blogs.map((blog: any) => (
-          <BlogCard key={blog.id} title={blog.title} content={blog.content} />
-        ))}
-      </section>
-    </main>
+    <>
+      <Nav />
+      <main>
+        <h1>Blogs</h1>
+        <section className='blogs-section'>
+          {blogs.map((blog: any) => (
+            <BlogCard key={blog.id} user_id={blog.user_id} title={blog.title} content={blog.content} />
+          ))}
+        </section>
+      </main>
+    </>
   )
 }
 
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
 import '../styles.css'
 
-export default function BlogCard({ title, content }: { title: string, content: string }) {
+interface Blog {
+  user_id: number
+  title: string
+  content: string
+}
+
+export default function BlogCard({ user_id, title, content }: Blog) {
+
+  const [owner, setOwner] = useState('')
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/users/${user_id}`)
+      .then(res => res.json())
+      .then(data => setOwner(data.username))
+      .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="blog-card">
+        <i>{owner}</i>
         <h2>{title}</h2>
         <p>{content}</p>
     </div>
